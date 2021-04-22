9352fab02ead56852ac5a273490432a1668be530
DESKTOP-RSQ6F5A\Mauri <mauriziodelmagno@gmail.com>
2021-04-22 19:32:51 +0200

    no message

--- Common/Model.SWHouseIntf.pas
+++ Common/Model.SWHouseIntf.pas
@@ -16,32 +16,43 @@ type
   end;
 
   ISWProduct = interface(IBaseNamedEntity)
+    procedure SetLicenseModels(val: TList<ISWLicenseModel>);
+    function GetLicenseModels(): TList<ISWLicenseModel>;
+    property LicenseModels: TList<ISWLicenseModel> read GetLicenseModels write SetLicenseModels;
   end;
 
   ISWHouseOperator = interface(IBaseUser)
   end;
 
-  ISWLicenseModel = interface(IBaseNamedEntity)
-    procedure SetSessionTokenExpirationMinutes(val: Integer);
-    function GetSessionTokenExpirationMinutes(): Integer;
-    property SessionTokenExpirationMinutes: Integer read GetSessionTokenExpirationMinutes write SetSessionTokenExpirationMinutes;
-    procedure SetLicenseTokenExpirationDays(val: Integer);
-    function GetLicenseTokenExpirationDays(): Integer;
-    property LicenseTokenExpirationDays: Integer read GetLicenseTokenExpirationDays write SetLicenseTokenExpirationDays;
-    procedure SetHowManyUsers(val: Integer);
-    function GetHowManyUsers(): Integer;
-    property HowManyUsers: Integer read GetHowManyUsers write SetHowManyUsers;
-    procedure SetPayload(val: String);
-    function GetPayload(): String;
-    property Payload: String read GetPayload write SetPayload;
-    procedure SetExpirationDays(val: Integer);
-    function GetExpirationDays(): Integer;
-    property ExpirationDays: Integer read GetExpirationDays write SetExpirationDays;
-  end;
-
   ISWHouseSession = interface(IBaseSession<ISWHouse, ISWHouseOperator>)
-  end;
+        end;
 
-implementation
+        ISWLicenseModel = interface(IBaseNamedEntity)
+          procedure SetLocalSessionServer(val: Boolean);
+          function GetLocalSessionServer(): Boolean;
+          property LocalSessionServer: Boolean read GetLocalSessionServer write SetLocalSessionServer;
+          procedure SetSessionTokenExpirationMinutes(val: Integer);
+          function GetSessionTokenExpirationMinutes(): Integer;
+          /// <semantics>Minuti di validità del SessionToken espresso in minuti, deve sempre essere minore del "LicenseTokenExpirationMInutes"</semantics>
+          property SessionTokenExpirationMinutes: Integer read GetSessionTokenExpirationMinutes write SetSessionTokenExpirationMinutes;
+          procedure SetLicenseTokenExpirationMinutes(val: Integer);
+          function GetLicenseTokenExpirationMinutes(): Integer;
+          /// <semantics>Minuti di validità del LicenseToken espresso in minuti, deve sempre essere maggiore della proprietà "SessionTokenExmirationMinutes"</semantics>
+          property LicenseTokenExpirationMinutes: Integer read GetLicenseTokenExpirationMinutes write SetLicenseTokenExpirationMinutes;
+          procedure SetHowManyUsers(val: Integer);
+          function GetHowManyUsers(): Integer;
+          /// <semantics>Numero di utenti/postazioni permessi</semantics>
+          property HowManyUsers: Integer read GetHowManyUsers write SetHowManyUsers;
+          procedure SetPayload(val: String);
+          function GetPayload(): String;
+          /// <semantics>Contenitore a disposizioine della SWHouse dovre poter memorizzare, ad es., impostazioni, autorizzazioni o altro</semantics>
+          property Payload: String read GetPayload write SetPayload;
+          procedure SetLicenseExpirationDays(val: Integer);
+          function GetLicenseExpirationDays(): Integer;
+          /// <semantics>Giorni di validità della licenza</semantics>
+          property LicenseExpirationDays: Integer read GetLicenseExpirationDays write SetLicenseExpirationDays;
+        end;
+
+      implementation
 
 end.
